8db2dfce04222f375f0d25af154cb32571707086
Ely Hess <ely.hess@me.com>
2020-10-12 18:10:38 -0600

    Add ship attributes test

---
+++ test/ship_test.rb
@@ -0,0 +1,18 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ship'
+
+class ShipTest < Minitest::Test
+
+  def test_it_exists_and_has_attributes
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_instance_of Ship, cruiser
+    assert_equal "Cruiser", cruiser.name
+    assert_equal 3, cruiser.length
+    assert_equal 3, cruiser.health
+    assert_equal false, cruiser.sunk?
+  end
+
+
+end
