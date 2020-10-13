27102d618f2fcdd5c26e938a82a876491bfcfda4
Ely Hess <ely.hess@me.com>
2020-10-12 20:54:22 -0600

    Add ship miss test

--- test/cell_test.rb
+++ test/cell_test.rb
@@ -39,4 +39,21 @@ class CellTest < Minitest::Test
     assert_equal 2, cell.ship.health
   end
 
+  def test_if_no_ship_it_misses
+    cell_1 = Cell.new("B4")
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_equal ".", cell_1.render
+    cell_1.fire_upon
+    assert_equal "M", cell_1.render
+  end
+
+
+
+
+
+
+
+
+
 end
