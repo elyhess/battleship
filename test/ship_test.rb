2e424d2548da20a72864ee0df8e9ddae8e482248
Ely Hess <ely.hess@me.com>
2020-10-12 18:12:36 -0600

    Add ship hit test

--- test/ship_test.rb
+++ test/ship_test.rb
@@ -14,5 +14,12 @@ class ShipTest < Minitest::Test
     assert_equal false, cruiser.sunk?
   end
 
+  def test_it_gets_hit
+    cruiser = Ship.new("Cruiser", 3)
+
+    assert_equal 3, cruiser.health
+    cruiser.hit
+    assert_equal 2, cruiser.health
+  end
 
 end
