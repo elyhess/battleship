6c8b0429ce71e5095f2572c71815e5ffb5013c65
Sam Yeo <yeosamuel95@gmail.com>
2020-10-15 16:59:08 -0600

    Add place method

--- lib/board.rb
+++ lib/board.rb
@@ -99,4 +99,12 @@ class Board
     end
   end
 
+  def place(ship, coordinates)
+    if valid_placement?(ship, coordinates)
+      coordinates.each do |cell|
+        @cells[cell].place_ship(ship) 
+      end
+    end
+  end
+
 end
