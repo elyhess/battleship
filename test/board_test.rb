0a42f01867f5d3f173cbc384dde1e0a0dbb893be
Sam Yeo <yeosamuel95@gmail.com>
2020-10-16 12:32:08 -0600

    Change cell.render to pass the last interaction pattern

--- test/board_test.rb
+++ test/board_test.rb
@@ -142,13 +142,32 @@ class BoardTest < Minitest::Test
   def test_board_renders
     board = Board.new
     cruiser = Ship.new("Cruiser", 3)
-    puts "\n"
     board.place(cruiser, ["A1", "A2", "A3"])
 
     expected_no_arg = "  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n"
     expected_with_arg = "  1 2 3 4 \nA S S S . \nB . . . . \nC . . . . \nD . . . . \n"
     assert_equal expected_no_arg, board.render
     assert_equal expected_with_arg, board.render(true)
+    submarine = Ship.new("Submarine", 2)
+    board.place(submarine, ["C1", "D1"])
+    board.cells["A1"].fire_upon
+    board.cells["B4"].fire_upon
+    board.cells["C1"].fire_upon
+    board.cells["D1"].fire_upon
+    expected_after_firing_no_arg =
+    "  1 2 3 4 \n" +
+    "A H . . . \n" +
+    "B . . . M \n" +
+    "C X . . . \n" +
+    "D X . . . \n"
+    expected_after_firing_with_arg =
+    "  1 2 3 4 \n" +
+    "A H S S . \n" +
+    "B . . . M \n" +
+    "C X . . . \n" +
+    "D X . . . \n"
+    assert_equal expected_after_firing_no_arg, board.render
+    assert_equal expected_after_firing_with_arg, board.render(true)
   end
 
 end
