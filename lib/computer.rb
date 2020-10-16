8ccc94b18527f2e01c791fbe7bb1e1b61e5f43bf
Ely Hess <ely.hess@me.com>
2020-10-16 16:55:58 -0600

    Add valid submarine coordinates method

--- lib/computer.rb
+++ lib/computer.rb
@@ -4,31 +4,52 @@ class Computer
   def initialize
     @board = Board.new
     @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
+    @letters = ["A", "B", "C", "D"]
+    @numbers = ["1", "2", "3", "4"]
   end
 
   def board_keys
     @board.cells.keys
   end
 
-  def valid_coordinates
+  def valid_cruiser_coordinates
     counter = 0
     coordinates = []
-    letters = ["A", "B", "C", "D"]
-    numbers = ["1", "2", "3", "4"]
 
     until board.valid_placement?(@cruiser, coordinates) == true
       coordinates = []
       @cruiser.length.times do
       counter += 1
-      coordinates  << letters.shuffle.first + numbers.shuffle.first
+      coordinates  << @letters.shuffle.first + @numbers.shuffle.first
       end
     end
     coordinates
   end
 
-  def place_ship(coordinates)
+  def valid_submarine_coordinates
+    counter = 0
+    coordinates = []
+
+    until board.valid_placement?(@submarine, coordinates) == true
+      coordinates = []
+      @submarine.length.times do
+      counter += 1
+      coordinates  << @letters.shuffle.first + @numbers.shuffle.first
+      end
+    end
+    coordinates
+  end
+
+  def place_cruiser(coordinates)
     coordinates.each do |coordinate|
       @board.cells[coordinate].place_ship(@cruiser)
+    end
+  end
+
+  def place_submarine(coordinates)
+    coordinates.each do |coordinate|
+      @board.cells[coordinate].place_ship(@submarine)
     end
   end
 
