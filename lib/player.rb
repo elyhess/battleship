8770a9f8597ed9c07d3755ee1942407adfed1a46
Sam Yeo <yeosamuel95@gmail.com>
2020-10-17 18:20:40 -0600

    Add player methods for placing ship and validation

--- lib/player.rb
+++ lib/player.rb
@@ -1,12 +1,56 @@
 class Player
-  attr_reader :board
+  attr_reader :board, :cruiser, :submarine
 
   def initialize
     @board = Board.new
+    @cruiser = Ship.new("Cruiser", 3)
+    @submarine = Ship.new("Submarine", 2)
   end
 
   def board_keys
     @board.cells.keys
+  end
+
+  def validate_cruiser_placement
+    puts "Enter the squares for the Cruiser (3 spaces):"
+    player_input = gets.chomp.upcase.split(" ").to_a # "A1 A2 A3"
+    until board.valid_placement?(@cruiser, player_input)
+      # Error message if bad placement
+      puts "Those are invalid coordinates. Please try again:" unless board.valid_placement?(@cruiser, player_input)
+      puts "Enter the squares for the Cruiser (3 spaces):"
+      player_input = gets.chomp.upcase.split(" ").to_a # ["A1", "A2", "A3"]
+    end
+    print "You have placed your Cruiser on #{player_input}.\n"
+    player_input
+  end
+
+  def validate_submarine_placement
+    puts "Enter the squares for the Submarine (2 spaces):"
+    player_input = gets.chomp.upcase.split(" ").to_a # "A1 A2"
+    until board.valid_placement?(@submarine, player_input)
+      # Error message if bad placement
+      puts "Those are invalid coordinates. Please try again:" unless board.valid_placement?(@cruiser, player_input)
+      puts "Enter the squares for the Submarine (2 spaces):"
+      player_input = gets.chomp.upcase.split(" ").to_a
+    end
+    print "You have placed your Submarine on #{player_input}.\n"
+    player_input
+  end
+
+  def place_cruiser
+    coords = validate_cruiser_placement
+    coords.each do |coord|
+      board.cells[coord].place_ship(@cruiser)
+    end
+    coords
+  end
+
+  def place_submarine
+    coords = validate_submarine_placement
+    coords.each do |coord|
+      board.cells[coord].place_ship(@submarine)
+    end
+    coords
   end
 
 end
